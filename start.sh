a8f5e032eb39f6ce5718085f57580d79608cd3c0
Amit Karamchandani Batra <amitkmbt@gmail.com>
2021-03-21 22:05:21 +0100

    Added start script for Linux and MacOS users

---
+++ start.sh
@@ -0,0 +1,1 @@
+python main.py -i 2 -o 5 --sample_rate 44100 --block_size 882 --audio_data_type "int32" --latency 1 --buffer_size 1 --normalization_threshold -35 --normalization_target -32 --normalization_profile "medium" --limiter_threshold -24
